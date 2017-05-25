b772daff7aed166c31653aeb8ab0f1ccdf0be96a
CatDTran <CatDTran@users.noreply.github.com>
2017-05-25 01:29:52 -0500

    Implemented method to get an instance of camera object

--- app/src/main/java/com/sweetroll/carrot/aibrows/CameraFragment.java
+++ app/src/main/java/com/sweetroll/carrot/aibrows/CameraFragment.java
@@ -1,7 +1,11 @@
 package com.sweetroll.carrot.aibrows;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,7 +19,7 @@ import android.view.ViewGroup;
 
 public class CameraFragment extends Fragment {
 
-    private final String TAG = "CameraFragment";
+    private static final String TAG = "CameraFragment";
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -37,5 +41,27 @@ public class CameraFragment extends Fragment {
         inflater.inflate(R.menu.fragment_camera_menu, menu);
     }
 
+    // Check if this device has a camera
+    private boolean checkCameraHardware(Context context) {
+        // this device has a camera
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            return true;
+        } else { // if this device does not
+            return false;
+        }
+    }
+
+    //A helper method which provides a safe way to get an instance of the Camera object
+    public static Camera getCameraInstance(){
+        Camera camera = null;
+        try {
+            camera = Camera.open(0); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+            Log.d(TAG, "Camera cannot be opend ");
+        }
+        return camera; // returns null if camera is unavailable
+    }
 
 }
