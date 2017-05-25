f61f90ed97d48f57f049eaf8671178abe57f4a98
CatDTran <CatDTran@users.noreply.github.com>
2017-05-25 00:59:35 -0500

    Implemented ViewPager. Enabled Options Menu.

--- app/src/main/java/com/sweetroll/carrot/aibrows/CameraFragment.java
+++ app/src/main/java/com/sweetroll/carrot/aibrows/CameraFragment.java
@@ -3,6 +3,9 @@ package com.sweetroll.carrot.aibrows;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -12,9 +15,27 @@ import android.view.ViewGroup;
 
 public class CameraFragment extends Fragment {
 
+    private final String TAG = "CameraFragment";
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstaceState){
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_camera, container, false);
+        setHasOptionsMenu(true);
         return rootView;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_camera_menu, menu);
+    }
+
+
 }
--- app/src/main/java/com/sweetroll/carrot/aibrows/MainActivity.java
+++ app/src/main/java/com/sweetroll/carrot/aibrows/MainActivity.java
@@ -1,13 +1,57 @@
 package com.sweetroll.carrot.aibrows;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+
+    private static final int NUM_PAGES = 3;
+    private ViewPager mViewPager;
+    private PagerAdapter mPagerAdapter;
+    private final String TAG = "FragmentActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Instantiate a ViewPager and a PagerAdapter
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mPagerAdapter = new MainPagerAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mPagerAdapter);
     }
+
+    //Override to define ViewPager behaviors when user press 'Back' button
+    @Override
+    public void onBackPressed(){
+        //if current page is the main (1st) screen, then call pop the back stack
+        if (mViewPager.getCurrentItem() == 0){
+            super.onBackPressed();
+        } else { //if not, back to the previous step
+            mViewPager.setCurrentItem(mViewPager.getCurrentItem() - 1);
+        }
+    }
+
+    //Implement the pager adapter class that will work with ViewPager here
+    private class MainPagerAdapter extends FragmentStatePagerAdapter{
+        public MainPagerAdapter(FragmentManager fragmentManager){
+            super(fragmentManager);
+        }
+
+        @Override
+        public Fragment getItem(int position){
+            return new CameraFragment();
+        }
+
+        @Override
+        public int getCount(){
+            return NUM_PAGES;
+        }
+    }
+
 }
